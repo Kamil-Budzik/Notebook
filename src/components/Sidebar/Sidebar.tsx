4d943d52d5ddefa18feb0d17ad257a7aff7a4c05
DonquixoteSabo <mixboksu@gmail.com>
2022-05-11 17:44:25 +0200

    feat: :sparkles: Add Sidebar

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { AiOutlinePlus } from 'react-icons/ai';
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <div className="flex items-center justify-between pb-8">
+        <h1 className="text-4xl font-bold">All Notes</h1>
+        <span>
+          <AiOutlinePlus className="cursor-pointer text-4xl transition hover:text-sky-500" />
+        </span>
+      </div>
+      <ul>
+        <li className="cursor-pointer py-2 text-lg transition hover:text-sky-500">
+          Drunken driving is a serious problem.
+        </li>
+        <li className="cursor-pointer py-2 text-lg transition hover:text-sky-500">
+          Drunken driving is a serious problem.
+        </li>
+        <li className="cursor-pointer py-2 text-lg transition hover:text-sky-500">
+          Drunken driving is a serious problem.
+        </li>
+        <li className="cursor-pointer py-2 text-lg transition hover:text-sky-500">
+          Drunken driving is a serious problem.
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,40 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+import Sidebar from '../components/Sidebar/Sidebar';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="mt-24 flex flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Notebook</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="w-100 m-auto flex max-w-[800px]">
+        <Sidebar />
+        <section className="w-3/5 px-12">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam quod
+          adipisci libero inventore ab ipsam cum deleniti officiis deserunt
+          expedita esse, autem totam! Impedit pariatur quis molestias! Quia, quo
+          dicta? Veritatis illo fugiat eum debitis qui! Distinctio pariatur
+          ducimus porro rem, soluta excepturi? Porro fugit officiis eligendi
+          accusantium ratione. Minus quam quisquam consequatur tenetur est non
+          delectus! Iusto, nam. Iusto. Quaerat molestiae, porro fugiat nemo
+          laborum sint inventore totam suscipit? Itaque recusandae nesciunt
+          cumque at molestiae, exercitationem ullam deserunt libero illum
+          quibusdam ad similique dolores aut dolorum aspernatur veritatis a.
+          Accusamus quidem quo delectus repudiandae tenetur asperiores,
+          praesentium, natus temporibus beatae quam voluptas excepturi earum
+          laborum commodi dolorem eos? Molestias cumque illum nulla voluptatibus
+          suscipit quasi quod ducimus ratione mollitia? Accusantium, nemo earum.
+          Dignissimos quos cum inventore ad laudantium eveniet quod commodi,
+          rerum similique, atque, aliquam possimus esse numquam tempora. Ipsa,
+          expedita. Natus nihil deleniti ipsa at unde dignissimos placeat!
+        </section>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
