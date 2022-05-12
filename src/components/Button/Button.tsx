12d38bbb0e856470df7a88349ed2950ba48491f1
DonquixoteSabo <mixboksu@gmail.com>
2022-05-12 18:37:21 +0200

    refactor: Allow Button to take default button attrs

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLButtonElement> {
   variant?: 'sky' | 'red';
   children: any;
 }
 
-const Button = ({ children, variant }: Props) => {
+const Button = ({ children, variant, ...props }: Props) => {
   const basicProperties =
     'py-3 px-10 text-2xl text-white transition rounded-md font-semibold';
 
   if (variant === 'red') {
     return (
-      <button className={`bg-rose-600 hover:bg-rose-700 ${basicProperties}`}>
+      <button
+        className={`bg-rose-600 hover:bg-rose-700 ${basicProperties}`}
+        {...props}
+      >
         {children}
       </button>
     );
   }
 
   return (
-    <button className={`bg-sky-500 hover:bg-cyan-600 ${basicProperties}`}>
+    <button
+      className={`bg-sky-500 hover:bg-cyan-600 ${basicProperties}`}
+      {...props}
+    >
       {children}
     </button>
   );
