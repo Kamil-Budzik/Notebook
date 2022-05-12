fa609068857b487239d4f8fd3c592607c86c43d5
DonquixoteSabo <mixboksu@gmail.com>
2022-05-12 17:55:08 +0200

    feat: Mock notes data

---
+++ src/data/mockedData.ts
@@ -0,0 +1,36 @@
+const mockedData = [
+  {
+    id: '1',
+    title: 'Do I have to do it right away?',
+    category: 'Category',
+    date: '9.05.2022',
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus optio deleniti numquam maxime vitae est. Quae quaerat reiciendis libero sequi illum a veritatis sed impedit labore maiores, ex assumenda. Officia.Eius, adipisci. Reprehenderit, reiciendis? Quas doloribus autem, voluptate aliquam dolor sed consequatur deserunt incidunt minus minima ipsam at magni, omnis maiores sapiente eos odit eum fugit hic eligendi, nam blanditiis.Magni aliquam odio, consequuntur eaque ex cupiditate facere autem distinctio assumenda porro, doloribus corrupti, eos sunt veniam pariatur illo aut rem commodi mollitia saepe. Totam iure similique odit magni harum. Soluta quam optio mollitia, rerum aperiam eligendi nisi amet repudiandae nemo quia deserunt provident a qui, doloremque harum repellat. Eius inventore sapiente hic obcaecati asperiores commodi animi rerum itaque quo! Quis fuga dolorum iure. Nostrum veritatis, tempora eligendi ad, molestiae, temporibus mollitia optio odit ipsum voluptas animi. Nisi, iste eius voluptates ipsum quae voluptatum numquam veniam nemo soluta molestias praesentium.',
+  },
+  {
+    id: '2',
+    title: 'Where did you go?',
+    category: 'Category',
+    date: '10.05.2022',
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus optio deleniti numquam maxime vitae est. Quae quaerat reiciendis libero sequi illum a veritatis sed impedit labore maiores, ex assumenda. Officia.Eius, adipisci. Reprehenderit, reiciendis? Quas doloribus autem, voluptate aliquam dolor sed consequatur deserunt incidunt minus minima ipsam at magni, omnis maiores sapiente eos odit eum fugit hic eligendi, nam blanditiis.Magni aliquam odio, consequuntur eaque ex cupiditate facere autem distinctio assumenda porro, doloribus corrupti, eos sunt veniam pariatur illo aut rem commodi mollitia saepe. Totam iure similique odit magni harum. Soluta quam optio mollitia, rerum aperiam eligendi nisi amet repudiandae nemo quia deserunt provident a qui, doloremque harum repellat. Eius inventore sapiente hic obcaecati asperiores commodi animi rerum itaque quo! Quis fuga dolorum iure. Nostrum veritatis, tempora eligendi ad, molestiae, temporibus mollitia optio odit ipsum voluptas animi. Nisi, iste eius voluptates ipsum quae voluptatum numquam veniam nemo soluta molestias praesentium.',
+  },
+  {
+    id: '3',
+    title: 'Was I wrong?',
+    category: 'Category',
+    date: '11.05.2022',
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus optio deleniti numquam maxime vitae est. Quae quaerat reiciendis libero sequi illum a veritatis sed impedit labore maiores, ex assumenda. Officia.Eius, adipisci. Reprehenderit, reiciendis? Quas doloribus autem, voluptate aliquam dolor sed consequatur deserunt incidunt minus minima ipsam at magni, omnis maiores sapiente eos odit eum fugit hic eligendi, nam blanditiis.Magni aliquam odio, consequuntur eaque ex cupiditate facere autem distinctio assumenda porro, doloribus corrupti, eos sunt veniam pariatur illo aut rem commodi mollitia saepe. Totam iure similique odit magni harum. Soluta quam optio mollitia, rerum aperiam eligendi nisi amet repudiandae nemo quia deserunt provident a qui, doloremque harum repellat. Eius inventore sapiente hic obcaecati asperiores commodi animi rerum itaque quo! Quis fuga dolorum iure. Nostrum veritatis, tempora eligendi ad, molestiae, temporibus mollitia optio odit ipsum voluptas animi. Nisi, iste eius voluptates ipsum quae voluptatum numquam veniam nemo soluta molestias praesentium.',
+  },
+  {
+    id: '4',
+    title: 'I made a mistake.',
+    category: 'Category',
+    date: '12.05.2022',
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus optio deleniti numquam maxime vitae est. Quae quaerat reiciendis libero sequi illum a veritatis sed impedit labore maiores, ex assumenda. Officia.Eius, adipisci. Reprehenderit, reiciendis? Quas doloribus autem, voluptate aliquam dolor sed consequatur deserunt incidunt minus minima ipsam at magni, omnis maiores sapiente eos odit eum fugit hic eligendi, nam blanditiis.Magni aliquam odio, consequuntur eaque ex cupiditate facere autem distinctio assumenda porro, doloribus corrupti, eos sunt veniam pariatur illo aut rem commodi mollitia saepe. Totam iure similique odit magni harum. Soluta quam optio mollitia, rerum aperiam eligendi nisi amet repudiandae nemo quia deserunt provident a qui, doloremque harum repellat. Eius inventore sapiente hic obcaecati asperiores commodi animi rerum itaque quo! Quis fuga dolorum iure. Nostrum veritatis, tempora eligendi ad, molestiae, temporibus mollitia optio odit ipsum voluptas animi. Nisi, iste eius voluptates ipsum quae voluptatum numquam veniam nemo soluta molestias praesentium.',
+  },
+];
+
+export default mockedData;
