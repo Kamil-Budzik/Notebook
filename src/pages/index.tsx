7cd4c06f35bd04eeab4e6be7655fcb12f4beb15a
DonquixoteSabo <mixboksu@gmail.com>
2022-05-12 17:55:19 +0200

    chore: Add Homepage introduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-import type { NextPage } from 'next';
-
+import React from 'react';
+import { BsDot } from 'react-icons/bs';
 import Layout from '../components/Layout/Layout';
 
-import NoteSection from '../components/NoteSection/NoteSection';
-import Sidebar from '../components/Sidebar/Sidebar';
-
-const Home: NextPage = () => {
+const HomePage = () => {
   return (
     <Layout>
-      <NoteSection />
+      <section className="px-12 md:w-3/5">
+        <header>
+          <h1 className="text-4xl font-bold">Welcome</h1>
+        </header>
+        <article className="my-24 text-2xl font-semibold">
+          <p>I will add app introduction/documentation here</p>
+        </article>
+      </section>
     </Layout>
   );
 };
 
-export default Home;
+export default HomePage;
